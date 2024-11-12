b5488fd152865d385babbf4558652de4bc5e6eaa
natsu <andrewtt824@gmail.com>
2024-11-11 22:14:02 -0500

    gran avance crear tablas y manejo de errores

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,37 +1,61 @@
-import java.io.FileReader;
+
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.File;
-import java.io.FileWriter;
 
 
 //com ousar system.out y system.flush
 public class App {
-   
+
     public static Boolean a = true;
-    
+    public static String input;
+    public static String in;
+    public static String line;
+
     public static void main(String[] args) throws Exception {
-       
+        System.out.println("Program Initialized");
         Scanner scanner = new Scanner(System.in);
-        while(a){
-            String comandoo = scanner.nextLine();
-            switch (comandoo) {
+        while (a) {
+            input = scanner.nextLine();
+
+            in = input.toLowerCase();
+            switchLabel:
+            switch (in) {
                 case "saludame":
-                Comando.saludame();
+                    Comando.saludame();
                     break;
-                    case "end":
+                case "end":
                     Comando.end();
-            
-                default:
                     break;
+                default:
+                    if (in.contains("create table")) {
+                        line = in.replace("create table", "").trim();
+                        line = line + ",";
+                        if(Comando.checkError(1)){
+                            break;
+                        };
+                        
+                        
+                        while (!input.contains(");")) {
+                            input = scanner.nextLine();
+                            line = line + input + ",";
+                            if(Comando.checkError(2)){
+                                
+                                break switchLabel;
+                            }
+                        }
+                        Comando.createTable(line);
+                    
+                    break;
+            } else {
+                System.out.println("Unknown command");
+                break;
             }
+
+
         }
-            
         
-        
-    
-        
-        
+
     }
+    scanner.close();
+}
+
 }
--- Main/src/Comando.java
+++ Main/src/Comando.java
@@ -1,10 +1,68 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
-
-public class Comando{
-    public static void saludame(){
+public class Comando {
+    public static boolean checkError(int x){
+        if(x==1){
+            if(App.line.equals("(,")|| App.line.equals( " (,")){
+                System.out.println("Error " + x +": El nombre de la tabla no fue especificado");
+               return true;
+            }
+            
+        }
+        if(x==2){
+            if(App.input.equals(");")){
+                return false;
+            }
+            if(App.input.split(" ").length!= 2){
+                System.out.println("Error "+ x +": No se especifico el nombre o el tipo de variable");
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    public static void saludame() {
         System.out.println("te saludo cv");
     }
-    public static void end(){
+
+    public static void end() {
+        System.out.print("Program ended");
         App.a = false;
     }
+
+    public static void createTable(String line) {
+        StringBuilder stringBuilder =new StringBuilder();
+        String[] in = line.split(",");
+        int x = in.length - 2;
+        String tableName = in[0].replace("(", "");
+       
+        String filepath = tableName + ".csv";
+
+        String[] temp = new String[x];
+        for (int i = 0; i < x; i++) {
+            temp[i] = in[i + 1];
+        }
+        String[] columnNames = new String[x];
+        String[] columnType = new String[x];
+
+        for (int i = 0; i < temp.length; i++) {
+            String[] parts = temp[i].trim().split(" ");
+            columnNames[i] = parts[0];
+            columnType[i] = parts[1];
+        }
+        for(String X : columnNames){
+            stringBuilder.append(X).append(",");
+            
+           }
+           
+        try (FileWriter fileWriter = new FileWriter(filepath)) {
+           fileWriter.write(stringBuilder.toString());
+           System.out.println("The table has been created successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
