82232170949325b0f52c50a77c57180715af8b74
Jose Gonzales <santijozer@gmail.com>
2024-11-23 07:41:55 -0500

    Insert casi listo

--- Main/src/Comando.java
+++ Main/src/Comando.java
@@ -6,33 +6,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Comando {
-   public static ArrayList<String> tableNames =new ArrayList<>();
-    public static void insert(String linea2, String linea1){
-        
+    public static ArrayList<String> tableNames = new ArrayList<>();
+
+    public static void insert(String linea2, String linea1) {
+
         linea2 = linea2.replace("(", "");
         linea2 = linea2.replace(")", "");
         linea2 = linea2.replace(",", "");
-        String[] line2 = linea2 .split(" ");
-
-        
-
+        String[] line2 = linea2.split(" ");
 
         linea1 = linea1.replace("(", "");
         linea1 = linea1.replace(")", "");
         linea1 = linea1.replace(",", "");
-        String[] line1 = linea1 .split(" ");
+        String[] line1 = linea1.split(" ");
 
-        
+        String[] cols = new String[line1.length - 3];
+        for (int i = 3; i < line1.length; i++) {
+            cols[i - 3] = line1[i];
 
-
-        
-
-        String[] cols = new String[line1.length-3];
-        for(int i = 3; i<line1.length; i++){
-            cols[i-3]=line1[i];
-            
         }
-     
+        String ColsNeeded = "";
+
+        for (String x : cols) {
+            ColsNeeded = ColsNeeded + x + " ";
+        }
+
         String fileCols = "";
         try {
             Scanner scanner = new Scanner(getFile(linea1));
@@ -40,62 +38,61 @@ public class Comando {
 
             scanner.close();
         } catch (FileNotFoundException e) {
-            
+
             e.printStackTrace();
         }
-        
-
-   
-
-        String ColsNeeded= "";
+        fileCols = fileCols.toLowerCase();
         String[] filesCols = fileCols.split(",");
-        for(String x : cols){
-            ColsNeeded = ColsNeeded + x + " ";
-        }
-        
-        
 
-      //DE ACA PARA ARRIBA TODO ESTA BUENO
-        
-        String line= "";
-        for(int i = 0; i<filesCols.length; i++){
-            int t = 0;
-            if(ColsNeeded.contains(filesCols[i])){
-                line = line + line2[t] + ",";
-                t= t+1;
-            }else{
-                line = line + ",";
+        for(String x: line2){
+            System.out.println(x);
+        }
+        String line = "";
+        int t = 0;
+        for (int i = 0; i < filesCols.length; i++) {
+            
+            if (ColsNeeded.contains(filesCols[i])) {
+                if(i!=0){
+                    line = line +   ", " + line2[t] ;
+                    t = t + 1;
+                }else{
+                    line=line+line2[t];
+                    t=t+1;
+                }
+                
+            } else {
+                if(i!=0){
+                    line =  " , "+line ;
+                }else{
+                    line= line+" ";
+                }
+                
             }
         }
-
-
-       
         System.out.println(line);
         FileWriter fileWriter;
         try {
-            fileWriter = new FileWriter(getName(linea1)+".csv");
-            fileWriter.write(line);
+            // Open the file in append mode by passing 'true' as the second argument
+            fileWriter = new FileWriter(getName(linea1) + ".csv", true);
+            fileWriter.write( "\n"+line ); // Add a newline to separate lines
+            fileWriter.close(); // Always close the writer after use
         } catch (IOException e) {
-            
             e.printStackTrace();
         }
-        
 
         System.out.println("Listo");
     }
 
-
-    
     public static void end() {
         System.out.print("Program ended");
         App.a = false;
     }
 
     public static void createTable(String line) {
-        
+
         StringBuilder stringBuilder = new StringBuilder();
         String[] in = line.split(",");
-        
+
         int x = in.length - 2;
         String tableName = in[0].replace("(", "").trim();
         tableNames.add(tableName);
@@ -116,9 +113,9 @@ public class Comando {
         for (String X : columnNames) {
             stringBuilder.append(X).append(",");
         }
-       stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         try (FileWriter fileWriter = new FileWriter(filepath)) {
-           
+
             fileWriter.write(stringBuilder.toString());
             System.out.println("The table has been created successfully");
         } catch (IOException e) {
@@ -126,7 +123,8 @@ public class Comando {
         }
 
     }
-    public static void help(){
+
+    public static void help() {
         System.out.println("Available Commands:");
         System.out.println("-------------------");
 
@@ -141,7 +139,7 @@ public class Comando {
         System.out.println("                - Replace 'table_name' with the name of the table.");
         System.out.println("                - Replace 'column_1', 'column_2', etc., with column names.");
         System.out.println("                - Replace 'datatype' with the data type of each column.");
-        
+
         System.out.println();
 
         // Command 2: END
@@ -153,15 +151,15 @@ public class Comando {
 
         // Prompt to type 'help' again
         System.out.println("Type 'help' anytime to see this list again.");
-     
+
     }
 
-    public static File getFile(String line){
+    public static File getFile(String line) {
         String[] lines = line.split(" ");
         File fileError = new File("");
-        for(int i = 0; i<lines.length; i++){
-            if(tableNames.contains(lines[i])){
-                File file = new File(lines[2]+".csv");
+        for (int i = 0; i < lines.length; i++) {
+            if (tableNames.contains(lines[i])) {
+                File file = new File(lines[2] + ".csv");
                 return file;
             }
         }
@@ -170,8 +168,8 @@ public class Comando {
 
     public static String getName(String line) {
         String[] lines = line.split(" ");
-        for(int i = 0; i<lines.length; i++){
-            if(tableNames.contains(lines[i])){
+        for (int i = 0; i < lines.length; i++) {
+            if (tableNames.contains(lines[i])) {
                 return lines[i];
             }
         }
